2f0b660e9635c087331ac02b61b1b3d83f55f7c1
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2023-09-08 19:04:48 +0530

    watermark-removed

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ const App = () => (
     scrollingSpeed={1500} /* Options here */
     navigation={true}
     navigationTooltips={['Section 1', 'Section 2', 'Section 3', 'Section 4', 'Section 5']}
-    showActiveTooltip={true}
+    showActiveTooltip={false}
     render={({ state, fullpageApi }) => {
       return (
         <ReactFullpage.Wrapper>
           {Array.apply(null, { length: 5 }).map((e, i) => (
             <Section className="section" id={i+1} fullpageApi={fullpageApi} key={i} url={
-              (i+1)%2===0 ? 'https://media.tenor.com/GfSX-u7VGM4AAAAC/coding.gif' : 'https://miro.medium.com/v2/resize:fit:960/1*5CZwLt-HSxgaztHi-lxs3g.gif'
+              (i)%2===0 ? 'https://media.tenor.com/GfSX-u7VGM4AAAAC/coding.gif' : 'https://miro.medium.com/v2/resize:fit:960/1*5CZwLt-HSxgaztHi-lxs3g.gif'
             }>
               id:{i+1}, fullpageApi: fullpageApi,
             </Section>
